65bc894dba80814fcc42ae1930eea941f11c2aaa
Gerlinde Christine Ciocea <44527090+gerlindee@users.noreply.github.com>
2019-05-23 05:20:27 +0300

    form for adding entry + displaying the table content (no update or delete yet)

--- laborator_7/welcome.php
+++ laborator_7/welcome.php
@@ -3,8 +3,70 @@
 
 	if (isset($_SESSION['User'])) {
 
-		echo "Welcome ".$_SESSION['User'].'<br/>';
-		echo '<a href="logout.php?Logout">Logout</a>';
+		?>
+
+		<!DOCTYPE html>
+		<html>
+		<head>
+			<title>URL Collection</title>
+			<link rel="stylesheet" type="text/css" href="style.css">
+		</head>
+
+		<body>
+			<div class="links">
+				<p>
+					<?php
+						echo "Welcome ".$_SESSION['User'].'<br/>';
+					?>
+				</p>
+				<p>
+					<?php
+					
+					echo '<a href="logout.php?Logout">(Logout)</a>';
+				?>
+				</p>
+			</div>
+
+			<?php
+				require_once('connection.php'); 
+				$query = "SELECT * FROM links";
+				$result = mysqli_query($connection, $query);
+			?>
+
+			<div>
+				<table class="links_table">
+					<thead>
+						<tr>
+							<th> Address </th>
+							<th> Description </th>
+							<th> Category </th>
+							<th colspan="2">Action</th>
+					</thead>
+
+					<?php
+						while ($row = $result->fetch_assoc()): ?>
+						<tr>
+							<td> <?php echo $row['address']; ?> </td>
+							<td> <?php echo $row['description']; ?> </td>
+							<td> <?php echo $row['category']; ?> </td>
+						</tr>
+					<?php endwhile; ?>
+				</table>
+			</div>
+
+			<div class="post_form">
+				<form action="table_process.php" method="POST">
+					<input type="text" name="url" placeholder="URL">
+					<input type="text" name="description" placeholder="Description">
+					<input type="text" name="category" placeholder="Category">
+					<input type="submit" name="Add" value="Add URL" id="btn">
+				</form>
+			</div>
+		</body>
+		</html>
+
+		<?php
+		
 	} else {
 
 		header("location:login.php");
