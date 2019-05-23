118ea5f9037be7dd72452c735c412a5017ac96dc
Gerlinde Christine Ciocea <44527090+gerlindee@users.noreply.github.com>
2019-05-23 22:09:28 +0300

    implemented update

--- laborator_7/welcome.php
+++ laborator_7/welcome.php
@@ -13,60 +13,67 @@
 		</head>
 
 		<body>
-			<div class="links">
-				<p>
-					<?php
-						echo "Welcome ".$_SESSION['User'].'<br/>';
-					?>
-				</p>
-				<p>
-					<?php
+			<form action="table_process.php" method="POST">
+				<div class="links">
+					<p>
+						<?php
+							echo "Welcome ".$_SESSION['User'].'<br/>';
+						?>
+					</p>
 					
-					echo '<a href="logout.php?Logout">(Logout)</a>';
+					<p>
+						<?php
+							echo '<a href="logout.php?Logout">(Logout)</a>';
+						?>
+					</p>
+				</div>
+
+				<?php
+					require_once('table_process.php');
+					$connection = mysqli_connect('localhost', 'root', '', 'laborator_7');
+					$query = "SELECT * FROM links";
+					$result = mysqli_query($connection, $query);
 				?>
-				</p>
-			</div>
 
-			<?php
-				require_once('connection.php'); 
-				$query = "SELECT * FROM links";
-				$result = mysqli_query($connection, $query);
-			?>
+				<div>
+					<table class="links_table">
+						<thead>
+							<tr>
+								<th style="width: 400px"> Address </th>
+								<th style="width: 400px"> Description </th>
+								<th> Category </th>
+								<th colspan="2">Action</th>
+							</tr>
+						</thead>
 
-			<div>
-				<table class="links_table">
-					<thead>
-						<tr>
-							<th> Address </th>
-							<th> Description </th>
-							<th> Category </th>
-							<th colspan="2">Action</th>
-					</thead>
+						<?php
+							while ($row = $result->fetch_assoc()): ?>
+							<tr>
+								<td> <?php echo $row['address']; ?> </td>
+								<td> <?php echo $row['description']; ?> </td>
+								<td> <?php echo $row['category']; ?> </td>
+								<td style="display: none"> <input type="text" name="id" style="width: 95%" value='<?php echo $row['id']; ?>'> </td>
+								<td> 
+									<a href="welcome.php?edit=<?php echo $row['id']; ?>" class = "edit_button"> Edit </a>
+									<a href="table_process.php?delete=<?php echo $row['id']; ?>" class="delete_button"> Delete </a>
+								</td>
+							</tr>
+						<?php endwhile; ?>
+					</table>
+				</div>
 
-					<?php
-						while ($row = $result->fetch_assoc()): ?>
-						<tr>
-							<td> <?php echo $row['address']; ?> </td>
-							<td> <?php echo $row['description']; ?> </td>
-							<td> <?php echo $row['category']; ?> </td>
-							<td> 
-								<a class="edit_button" href="index.php?edit=<?php echo $row['id']; ?>"> Edit </a>
-
-								<a href="table_process.php?delete=<?php echo $row['id']; ?>" class="delete_button"> Delete </a>
-							</td>
-						</tr>
-					<?php endwhile; ?>
-				</table>
-			</div>
-
-			<div class="post_form">
-				<form action="table_process.php" method="POST">
-					<input type="text" name="url" placeholder="URL">
-					<input type="text" name="description" placeholder="Description">
-					<input type="text" name="category" placeholder="Category">
-					<input type="submit" name="Add" value="Add URL" id="btn">
-				</form>
-			</div>
+				<div class="post_form">
+					<input type="hidden" name="id" value="<?php echo $id; ?>">
+					<input type="text" name="url" value="<?php echo $address?>" placeholder="URL">
+					<input type="text" name="description" value="<?php echo $description?>" placeholder="Description">
+					<input type="text" name="category" value="<?php echo $category?>" placeholder="Category">
+					<?php if ($update == true): ?>
+						<input type="submit" name="Update" value="Update" id="btn">
+					<?php else: ?>
+						<input type="submit" name="Add" value="Save" id="btn">
+					<?php endif; ?>
+				</div>
+			</form>
 		</body>
 		</html>
 
